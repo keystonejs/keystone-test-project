86ede6b645228df8203dcb09a0426153e5d28a29
Jed Watson <jed.watson@me.com>
2015-08-26 12:02:59 +1000

    Improving the getting started experience

--- client/scripts/application.js
+++ client/scripts/application.js
@@ -1,9 +1,37 @@
 var React = require('react');
 
+var { Button } = require('elemental');
+
 var App = React.createClass({
-	render: function() {
+	componentDidMount () {
+		React.findDOMNode(this.refs.btn).focus();
+	},
+	render () {
 		return (
-			<div>Hello World</div>
+			<div className="box">
+				<img id="logo" src="/images/logo.png" width="205" height="68" alt="KeystoneJS" />
+				<h3>Welcome to KeystoneJS</h3>
+				<p className="welcome">An admin user has been created for you:</p>
+				<div className="details">
+					<div className="label">
+						Email address:
+					</div>
+					<div className="value">
+						<code>user@keystonejs.com</code>
+					</div>
+				</div>
+				<div className="details">
+					<div className="label">
+						Password:
+					</div>
+					<div className="value">
+						<code>admin</code>
+					</div>
+				</div>
+				<div className="toolbar">
+					<Button ref="btn" href="/keystone">Open KeystoneJS</Button>
+				</div>
+			</div>
 		);
 	}
 });
