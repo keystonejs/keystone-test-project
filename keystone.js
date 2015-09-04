4ce86db16213e6e277bf8efe0d880060d6d93fb3
Jed Watson <jed.watson@me.com>
2015-09-04 23:46:12 +1000

    Adding cloudinary account from Keystone's demo site

--- keystone.js
+++ keystone.js
@@ -11,6 +11,8 @@ keystone.init({
 	'views': 'templates/views',
 	'view engine': 'jade',
 
+	'cloudinary config': 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo',
+
 	'auto update': true,
 	'session': true,
 	'auth': true,
