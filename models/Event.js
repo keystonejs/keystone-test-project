3b6ec3a282e419f9fe6c87744e1eaf47907109d6
Joss Mackison <joss.mackison@gmail.com>
2015-09-07 10:30:39 +1000

    Add Event/Job models as orphan lists

---
+++ models/Event.js
@@ -0,0 +1,39 @@
+const keystone = require('keystone');
+const Types = keystone.Field.Types;
+
+var Event = new keystone.List('Event', {
+	autokey: { from: 'name', path: 'key', unique: true },
+	track: true
+});
+
+var deps = {
+	videoEmbed: { videoEmbed: true, videoEmbedData: { exists: true } }
+};
+
+Event.add({
+	name: { type: String, initial: true, required: true, index: true },
+	eventType: { type: Types.Select, options: [ 'workshop', 'retreat', 'course', 'festival' ], index: true },
+	eventState: { type: Types.Select, options: 'new, draft, published, suspended, archived', index: true },
+	startDate: { type: Types.Date, initial: true, required: true },
+	endDate: { type: Types.Date, initial: true },
+});
+
+Event.add('Contact Information', {
+	phone: { type: String, width: 'short' },
+	website: { type: Types.Url, collapse: true },
+	location: { type: Types.Location, collapse: true, initial: true, required: ['suburb'] },
+	bookingUrl: { type: Types.Url, collapse: true }
+});
+
+Event.add('Description', {
+	price: { type: String, collapse: true },
+	content: {
+		brief: { type: Types.Html, wysiwyg: true, height: 150 },
+		summary: { type: Types.Html, hidden: true },
+		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+	},
+	schedule: { type: Types.Html, wysiwyg: true, collapse: true }
+});
+
+Event.defaultColumns = 'name, eventType|15%, eventState|15%, startDate|15%';
+Event.register();
---
+++ models/Job.js
@@ -0,0 +1,44 @@
+const keystone = require('keystone');
+const Types = keystone.Field.Types;
+
+var Job = new keystone.List('Job', {
+	autokey: { from: 'name', path: 'key', unique: true },
+	track: true
+});
+
+Job.set('notes', {
+	isFeatured: 'Featured jobs are displayed first on the home page'
+});
+
+Job.add({
+	name: { type: String, initial: true, required: true, index: true },
+	jobState: { type: Types.Select, options: 'draft, published, filled, archived', default: 'draft', index: true },
+	jobType: { type: Types.Select, options: [ 'full-time', 'part-time', 'casual', 'other' ], index: true },
+	jobRole: { type: Types.Select, options: [ 'accounting', 'admin', 'customer-service', 'human-resources', 'information-technology', 'sales-marketing', 'other' ], index: true },
+	isFeatured: { type: Boolean, index: true },
+	startDate: { type: Types.Date },
+	publishedOn: { type: Types.Date, default: Date.now, noedit: true, index: true },
+});
+
+Job.add('Contact Information', {
+	email: { type: Types.Email },
+	phone: { type: String, width: 'short' },
+	phoneViews: { type: Types.Number },
+	website: { type: Types.Url, collapse: true },
+	location: { type: Types.Location, collapse: true }
+});
+
+Job.add('Description', {
+	pay: { type: Types.Money },
+	payPer: { type: Types.Select, options: [ 'hour', 'week', 'fortnight', 'month', 'year' ], index: true },
+	payDescription: { type: Types.Textarea, collapse: true },
+	imageCaption: { type: String, collapse: true },
+	content: {
+		brief: { type: Types.Html, wysiwyg: true, height: 150 },
+		summary: { type: Types.Html, hidden: true },
+		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+	}
+});
+
+Job.defaultColumns = 'name, jobState|15%, jobType|15%, jobRole|15%';
+Job.register();
