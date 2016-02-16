ac78e25f0c483f74fa17049a35292dbea51df53d
Jed Watson <jed.watson@me.com>
2016-02-16 16:45:57 +1100

    Finishing Test 1-User

--- tests/api-react/user/1-User.js
+++ tests/api-react/user/1-User.js
@@ -1,3 +1,4 @@
+import Domify from 'react-domify';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Col, Form, FormField, FormInput, Row } from 'elemental';
@@ -7,6 +8,7 @@ import api from '../../../client/lib/api';
 const Test = React.createClass({
 	getInitialState () {
 		return {
+			action: 'Start Test',
 			data: {
 				name: '',
 				email: '',
@@ -24,29 +26,37 @@ const Test = React.createClass({
 			json: this.state.data,
 		}, (err, res, body) => {
 			this.props.result('Received response:', body);
-			this.props.assert('status code is 400').truthy(res.statusCode === 400);
-			this.props.complete();
+			this.props.assert('status code is 400').truthy(() => res.statusCode === 400);
+			this.props.assert('error is "validation errors"').truthy(() => body.error === 'validation errors');
+			if (!this.state.data.password) {
+				this.props.assert('name is required').truthy(() => body.detail.name.type === 'required');
+				this.props.assert('email is required').truthy(() => body.detail.email.type === 'required');
+				this.props.assert('password is required').truthy(() => body.detail.password.type === 'required');
+				this.setState({
+					action: 'Continue Test',
+					data: {
+						'name.full': 'first last',
+						'email': 'not an email',
+						'password': 'abcd1234',
+						'password_confirm': 'abcd',
+					},
+				});
+			} else {
+				this.props.assert('email is required').truthy(() => body.detail.email.type === 'invalid');
+				this.props.assert('passwords don\'t match').truthy(() => body.detail.password.type === 'invalid');
+				this.props.complete();
+			}
 		});
 	},
 	render () {
 		return (
 			<div>
 				<h2 style={{ marginBottom: 0 }}>Create User</h2>
-				<Form type="horizontal" style={{ marginTop: 40 }}>
-					<FormField label="Name:">
-						<FormInput />
-					</FormField>
-					<FormField label="Email address:">
-						<FormInput />
-					</FormField>
-					<FormField label="Password:">
-						<FormInput />
-					</FormField>
-				</Form>
+				<Domify style={styles.data} value={this.state.data} />
 				<hr />
 				<Row>
 					<Col sm="1/2">
-						<Button ref="run" type="primary" onClick={this.runTest}>Test Create User</Button>
+						<Button ref="run" type="primary" onClick={this.runTest}>{this.state.action}</Button>
 					</Col>
 					<Col sm="1/2" style={{ align: 'right' }}>
 						<Button ref="next" type="default" onClick={this.props.next} style={{ float: "right" }}>Next</Button>
@@ -56,5 +66,13 @@ const Test = React.createClass({
 		);
 	}
 });
+
+const styles = {
+	data: {
+		fontFamily: 'Monaco',
+		fontSize: '0.9em',
+		marginTop: 20,
+	},
+};
 
 module.exports = Test;
