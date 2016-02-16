d58f6025763eedad1bae6c7f3f38af20f026bc7e
Jed Watson <jed.watson@me.com>
2016-02-16 16:45:16 +1100

    User password should be required

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ var User = new keystone.List('User');
 User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true, unique: true },
-	password: { type: Types.Password, initial: true },
+	password: { type: Types.Password, initial: true, required: true },
 	company: { type: Types.Relationship, ref: 'Company', initial: true, index: true },
 	address: { type: Types.Location, collapse: true },
 }, 'Permissions', {
