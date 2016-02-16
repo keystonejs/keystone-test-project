cd1ac9d2ff45b4fd8cd98d1b5e0d061a33bbfc45
Jed Watson <jed.watson@me.com>
2016-02-16 16:46:08 +1100

    Finishing Test 2-User

--- tests/api-react/user/2-User.js
+++ tests/api-react/user/2-User.js
@@ -1,16 +1,20 @@
+import Domify from 'react-domify';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Col, Form, FormField, FormInput, Row } from 'elemental';
 
 import api from '../../../client/lib/api';
+import clone from '../../../client/lib/clone';
 
 const Test = React.createClass({
 	getInitialState () {
 		return {
+			action: 'Start Test',
 			data: {
-				'name.full': 'Vito Belgiorno-Zegna',
-				email: 'test-' + Date.now() + '@keystonejs.com',
+				'name.full': 'Test User',
+				email: 'user@keystonejs.com',
 				password: 'test1234',
+				password_confirm: 'test1234',
 			},
 		};
 	},
@@ -24,26 +28,26 @@ const Test = React.createClass({
 			json: this.state.data,
 		}, (err, res, body) => {
 			this.props.result('Received response:', body);
-			this.props.assert('status code is 200').truthy(res.statusCode === 200);
-			this.props.assert('email returned successfully').truthy(body.fields.email === this.state.data.email);
-			this.props.complete();
+			if (this.state.data.email === 'user@keystonejs.com') {
+				this.props.assert('status code is 500').truthy(() => res.statusCode === 500);
+				this.props.assert('error is "database error"').truthy(() => body.error === 'database error');
+				this.setState({
+					data: clone(this.state.data, {
+						email: 'user-' + Date.now() + '@keystonejs.com',
+					}),
+				});
+			} else {
+				this.props.assert('status code is 200').truthy(() => res.statusCode === 200);
+				this.props.assert('email returned successfully').truthy(() => body.fields.email === this.state.data.email);
+				this.props.complete({ user: body });
+			}
 		});
 	},
 	render () {
 		return (
 			<div>
 				<h2 style={{ marginBottom: 0 }}>Create User</h2>
-				<Form type="horizontal" style={{ marginTop: 40}}>
-					<FormField label="Name">
-						<FormInput defaultValue={this.state.data.name} />
-					</FormField>
-					<FormField label="Email address">
-						<FormInput defaultValue={this.state.data.email} />
-					</FormField>
-					<FormField label="Password">
-						<FormInput defaultValue={this.state.data.password} />
-					</FormField>
-				</Form>
+				<Domify style={styles.data} value={this.state.data} />
 				<hr />
 				<Row>
 					<Col sm="1/2">
@@ -57,5 +61,13 @@ const Test = React.createClass({
 		);
 	}
 });
+
+const styles = {
+	data: {
+		fontFamily: 'Monaco',
+		fontSize: '0.9em',
+		marginTop: 20,
+	},
+};
 
 module.exports = Test;
