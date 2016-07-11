1f7649824bc2b5470c7206a2e9872909e64e0127
Joseph Gentle <me@josephg.com>
2016-07-11 12:56:47 +1000

    Tweaked keystone.Storage definition

--- models/File.js
+++ models/File.js
@@ -1,13 +1,26 @@
 var keystone = require('keystone');
 var transform = require('model-transform');
+var path = require('path');
 var Types = keystone.Field.Types;
 
 var File = new keystone.List('File');
 
 var UploadStore = new keystone.Storage({
 	adapter: keystone.Storage.Adapters.FS,
-	path: 'uploads',
+	path: keystone.expandPath('uploads'),
+	// path: '../uploads',
+	// path: path.normalize(__dirname + '/../uploads'),
 	publicPath: '/files/uploads/',
+	schema: {
+		path: true,
+		originalname: true,
+	},
+
+	// default: random, retry.
+
+	generateFilename: keystone.Storage.filenameFromHash, // or filenameFromUpload filenameFromRandom
+	// generateFilename: (item, file, i) => file.originalname,
+	whenExists: 'overwrite', // 'error' 'retry'
 });
 
 File.add({
