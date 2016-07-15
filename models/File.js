8d27a8310b2a58f58694b41fae91abc638e3226a
Jed Watson <jed.watson@me.com>
2016-07-15 23:36:22 +1000

    Cleanup

--- models/File.js
+++ models/File.js
@@ -1,6 +1,5 @@
 var keystone = require('keystone');
 var transform = require('model-transform');
-var path = require('path');
 var Types = keystone.Field.Types;
 
 var File = new keystone.List('File');
@@ -9,50 +8,13 @@ var storage = new keystone.Storage({
 	adapter: keystone.Storage.Adapters.FS,
 	fs: {
 		path: keystone.expandPath('./uploads'),
-		permissionMask: 0644,
-		publicUrlBase: '/public/uploads'
+		urlPrefix: '/public/uploads/',
 	},
-
-	// generateFilename: keystone.Storage.filenameFromHash, // or filenameFromUpload filenameFromRandom
-
-	// generateFilename: (file, i) => `${inflect.slug(file.originalName)}}`,
-	// onConflict: 'retry', // 'retry' | 'overwrite' | 'error'
-
-	// adapter: keystone.Storage.Adapters.S3({
-	// 	path: 'keystone-uploads',
-	// 	bucket: 'my-bucket',
-	// 	accessKey: 'ABASdfasdfasd',
-	// 	secretAccessKey: 'asdfasdfi87a6sdf9',
-	// 	policy: 'ACL_PRIVATE',
-	// 	region: 'ap-southeast-2',
-	// 	filePath: (doc, filehash, originalName) => `${filehash}`,
-	// 	onConflict: 'overwrite', // 'overwrite' | 'rename' | 'random' | 'ordinal',
-	// }),
-	// adapter: keystone.Storage.Adapters.mongoBlobStore({}),
-	// adapter: keystone.Storage.Adapters.inMemory({}),
-	// adapter: keystone.Storage.Adapters.redisBlobStore({ host: 'localhost', port: 6379, dbNumber: 0 }),
-
-	// getPublicUrl(fileLocator) >> url
-	// upload(file) >> fileName, fileLocator
-	// download(fileLocator) >> fileStream
-	// remove(fileLocator)
-	// exists(filename, callback)
-
-	// maxSize:
-	// allowedMimeType:
-	// allowedFilenameRegex:  // ??
-
-	// adapter.getPublicPath()
 	schema: {
-
 		mimetype: false,
 		size: false,
 		originalname: true,
-
-		// etag: true,
-		// serverTime: true,
 	},
-
 });
 
 File.add({
