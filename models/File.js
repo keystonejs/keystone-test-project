908ccd23add55b034cf4f81e049d0d1db67059ea
Joseph Gentle <me@josephg.com>
2016-07-15 15:52:17 +1000

    More clarity over keystone storage options & configuration

--- models/File.js
+++ models/File.js
@@ -5,27 +5,59 @@ var Types = keystone.Field.Types;
 
 var File = new keystone.List('File');
 
-var UploadStore = new keystone.Storage({
+var storage = new keystone.Storage({
 	adapter: keystone.Storage.Adapters.FS,
-	path: keystone.expandPath('uploads'),
-	// path: '../uploads',
-	// path: path.normalize(__dirname + '/../uploads'),
-	publicPath: '/files/uploads/',
-	schema: {
-		path: true,
-		originalname: true,
+	fs: {
+		path: keystone.expandPath('./uploads'),
+		permissionMask: 0644,
+		publicUrlBase: '/public/uploads'
 	},
 
-	// default: random, retry.
+	// generateFilename: keystone.Storage.filenameFromHash, // or filenameFromUpload filenameFromRandom
 
-	generateFilename: keystone.Storage.filenameFromHash, // or filenameFromUpload filenameFromRandom
-	// generateFilename: (item, file, i) => file.originalname,
-	whenExists: 'overwrite', // 'error' 'retry'
+	// generateFilename: (file, i) => `${inflect.slug(file.originalName)}}`,
+	// onConflict: 'retry', // 'retry' | 'overwrite' | 'error'
+
+	// adapter: keystone.Storage.Adapters.S3({
+	// 	path: 'keystone-uploads',
+	// 	bucket: 'my-bucket',
+	// 	accessKey: 'ABASdfasdfasd',
+	// 	secretAccessKey: 'asdfasdfi87a6sdf9',
+	// 	policy: 'ACL_PRIVATE',
+	// 	region: 'ap-southeast-2',
+	// 	filePath: (doc, filehash, originalName) => `${filehash}`,
+	// 	onConflict: 'overwrite', // 'overwrite' | 'rename' | 'random' | 'ordinal',
+	// }),
+	// adapter: keystone.Storage.Adapters.mongoBlobStore({}),
+	// adapter: keystone.Storage.Adapters.inMemory({}),
+	// adapter: keystone.Storage.Adapters.redisBlobStore({ host: 'localhost', port: 6379, dbNumber: 0 }),
+
+	// getPublicUrl(fileLocator) >> url
+	// upload(file) >> fileName, fileLocator
+	// download(fileLocator) >> fileStream
+	// remove(fileLocator)
+	// exists(filename, callback)
+
+	// maxSize:
+	// allowedMimeType:
+	// allowedFilenameRegex:  // ??
+
+	// adapter.getPublicPath()
+	schema: {
+
+		mimetype: false,
+		size: false,
+		originalname: true,
+
+		// etag: true,
+		// serverTime: true,
+	},
+
 });
 
 File.add({
 	name: { type: String },
-	file: { type: Types.File, storage: UploadStore, required: true, initial: true },
+	file: { type: Types.File, storage: storage, required: true, initial: true },
 });
 
 transform.toJSON(File);
