fdeba4b7d817aa22c579838f5262408cec7fb344
Jed Watson <jed.watson@me.com>
2016-07-27 16:35:14 +1000

    Restoring tests

--- tests/api-react/index.js
+++ tests/api-react/index.js
@@ -1,10 +1,10 @@
 module.exports = [
-	// require('./user/1-User'), // Tests User validation
-	// require('./user/2-User'), // Creates a new User
-	// require('./user/3-User'), // Updates the User
-	// require('./user/4-User'), // Deletes the User
-	// require('./image/1-Image'), // Creates a Gallery and uploads an image
-	// require('./image/2-Image'), // Updates the Gallery with multiple images
-	// require('./image/3-Image'), // Reorders Gallery images then removes them
+	require('./user/1-User'), // Tests User validation
+	require('./user/2-User'), // Creates a new User
+	require('./user/3-User'), // Updates the User
+	require('./user/4-User'), // Deletes the User
+	require('./image/1-Image'), // Creates a Gallery and uploads an image
+	require('./image/2-Image'), // Updates the Gallery with multiple images
+	require('./image/3-Image'), // Reorders Gallery images then removes them
 	require('./file/1-File'), // Creates a new File item and uploads a file
 ];
