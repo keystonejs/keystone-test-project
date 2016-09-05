75e2c6484a050705faa1b5b365c7fafdbe093741
Jed Watson <jed.watson@me.com>
2016-09-06 00:23:42 +1000

    Fixing api test harness signin bouncer

--- client/lib/api.js
+++ client/lib/api.js
@@ -6,7 +6,16 @@ const api = {
 	post (url, options = {}, callback) {
 		if (!options.headers) options.headers = {};
 		options.headers[Keystone.csrf_header_key] = Keystone.csrf_token_value;
-		xhr.post(url, options, callback);
+		xhr.post(url, options, (err, res, body) => {
+			// Handle Unauthorized responses by redirecting to the signin page
+			if (res && res.statusCode === 401) {
+				alert('Please sign in to run the API Tests');
+				var from = window.location.pathname;
+				window.location.href = '/keystone/signin?from=' + from;
+			} else {
+				callback(err, res, body);
+			}
+		});
 	},
 };
 
