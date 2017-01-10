1e16ee0a4cafe592dfb9be3a9d84073b41c91d3c
Jed Watson <jed.watson@me.com>
2017-01-10 15:51:51 +1100

    Switching to pug, jade's been deprecated for long enough now!

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.options = {
 	'static': 'public',
 	'favicon': 'public/favicon.ico',
 	'views': 'templates/views',
-	'view engine': 'jade',
+	'view engine': 'pug',
 
 	'cloudinary config': 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo',
 
